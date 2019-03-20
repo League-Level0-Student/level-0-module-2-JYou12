700e6ce70e0f9dbbb293e1bb706ad5e7249aa703
Joshua You <JYou12@users.noreply.github.com>
2019-03-20 11:23:57 -0700

    Update Rollercoaster.java

--- src/random/Rollercoaster.java
+++ src/random/Rollercoaster.java
@@ -2,7 +2,7 @@ package random;
 
 import javax.swing.JOptionPane;
 
-public class RollerCoaster {
+public class Rollercoaster {
 public static void main(String[] args) {
 	
 	String height = JOptionPane.showInputDialog("How tall are you in inches");
